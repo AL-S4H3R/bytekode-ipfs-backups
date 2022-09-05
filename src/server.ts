510a61cfea4010ee5bc2f4fa1e031f640640e613
Shlok D. Mange <shlok_mange@protonmail.com>
2022-09-05 11:25:53 +0530

    project init

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
-console.log('Hello world')
+import { config } from 'dotenv'
+import { S3 } from 'aws-sdk'
+import { schedule } from 'node-cron'
+
+config()
+
+const API_URL = `https://s3.filebase.com`
+const FILEBASE_ACCESS_KEY = process.env.FILEBASE_ACCESS_KEY as string
+const FILEBASE_SECRET_KEY = process.env.FILEBASE_SECRET_KEY as string
+
+const awsClient = new S3({
+    signatureVersion: 'v4',
+    endpoint: API_URL,
+    credentials: {
+        accessKeyId: FILEBASE_ACCESS_KEY,
+        secretAccessKey: FILEBASE_SECRET_KEY
+    }
+})
+
+const listAllBuckets = async () => {
+    awsClient.listBuckets((err, data) => {
+        console.log(data.Buckets![0]['Name'])
+    })
+}
+
+const uploadToBucket = async () => {
+    
+}
+
+schedule('* * * * 1', () => console.log('Running'))
