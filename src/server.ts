39fc5996ee181d0d239b0964a56fb9479f4cf98f
Shlok D. Mange <shlok_mange@protonmail.com>
2022-09-15 17:46:49 +0530

    delete unwanted comments

--- src/server.ts
+++ src/server.ts
@@ -24,69 +24,7 @@ const supabaseClient = createClient(
     SUPABASE_SECRET_KEY    
 )
 
-const listAllBuckets = async () => {
-    awsClient.listBuckets((err, data) => {
-        console.log(data.Buckets![0]['Name'])
-    })
-}
-
-const uploadToBucket = async (filename: string) => {
-    awsClient.putObject({
-        Bucket: 'bytekode-test-bucket',
-        Key: `contracts/${filename}`,
-        Body: 'Test',
-        ContentType: 'text/plain'
-    }, (err, data) => {
-        console.log(data)
-    })
-}
-
-const getDatabaseDetails = async () => {
-    let histories = ``
-    let contract = ``
-    const { data: contracts, error: conErr } = await supabaseClient.from('contracts').select('*')
-    console.log(contracts)
-    if(conErr){
-        console.log(`Error fetching contracts`)
-    }
-    const { data: history, error: histErr } = await supabaseClient.from('history').select('*')
-    console.log(history)
-    if(histErr){
-        console.log(`Error fetching decoding history`)
-    }
-    /*
-    
-    history?.map((tx, index) => {
-        console.log(JSON.stringify(tx))
-        histories += JSON.stringify(tx)
-    })
-    contracts?.map((tx, index) => {
-        console.log(JSON.stringify(tx))
-        contract += JSON.stringify(tx)
-    })
-    awsClient.putObject({
-        Bucket: 'bytekode-test-bucket',
-        Key: `contracts/${Date.now()}`,
-        Body: contract,
-        ContentType: 'text/plain'
-    }, (err, data) => {
-        if(!err){
-            awsClient.putObject({
-                Bucket: 'bytekode-test-bucket',
-                Key: `history/${Date.now()}`,
-                Body: histories,
-                ContentType: 'text/plain'  
-            })
-        }
-    })
-    */
-}
-
-// uploadToBucket('TestContract')
-// getDatabaseDetails()
-// runs every minute
 // schedule('* * * * 1', () => console.log('Running'))
-
 const contractsBackup = async () => {
     const { data: contracts, error: conErr } = await supabaseClient.from('contracts').select('*')
     contracts && console.log(JSON.stringify(contracts))
